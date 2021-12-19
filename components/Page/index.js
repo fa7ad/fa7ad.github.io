4f6bbe6690c67f2302e5e59b575ba9ba82869629
Fahad Hossain <set.name.fahad@gmail.com>
2021-12-19 23:13:14 +0100

    chore: remove prism completely

--- components/Page/index.js
+++ components/Page/index.js
@@ -1,5 +1,4 @@
 import clsx from 'clsx'
-import Script from 'next/script'
 
 import Header from './_header'
 import Footer from './_footer'
@@ -11,27 +10,9 @@ const Page = ({ children, className }) => {
   return (
     <>
       <Metadata />
-
       <Header />
       <main className={clsx(styles.main, className)}>{children}</main>
       <Footer />
-
-      <Script
-        strategy='afterInteractive'
-        src='https://cdn.jsdelivr.net/npm/prismjs@1.25.0/components/prism-core.min.js'
-      />
-      <Script
-        strategy='afterInteractive'
-        src='https://cdn.jsdelivr.net/npm/prismjs@1.25.0/plugins/autoloader/prism-autoloader.min.js'
-      />
-      <Script
-        strategy='afterInteractive'
-        src='https://cdn.jsdelivr.net/npm/prismjs@1.25.0/plugins/show-language/prism-show-language.min.js'
-      />
-      <Script
-        strategy='afterInteractive'
-        src='https://cdn.jsdelivr.net/npm/prismjs@1.25.0/plugins/line-numbers/prism-line-numbers.min.js'
-      />
     </>
   )
 }
