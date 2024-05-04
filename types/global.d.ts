572a6ec14c86cae806217a478724734f426257f1
Fahad Hossain <setname.fahad@gmail.com>
2024-05-04 02:22:24 +0200

    chore: remove old unused types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,6 @@
 import { type Metadata } from 'next'
 
 declare global {
-  type Entry = 'posts' | 'pages'
-
   interface RawMetadata {
     title: string
     date: string
@@ -23,20 +21,6 @@ declare global {
     series?: SeriesMap[string]
   }
 
-  interface UnprocessedPage {
-    title: string
-    file: string
-    slug: string
-  }
-
-  interface ProcessedPage {
-    title: string
-    slug: string
-    content: string
-    shortExcerpt: string
-    longExcerpt: string
-  }
-
   interface SeriesMap {
     [key: string]: {
       title: string
@@ -46,15 +30,6 @@ declare global {
       }[]
     }
   }
-
-  type UnprocessedEntry = UnprocessedPost | UnprocessedPage
-  type ProcessedEntry = ProcessedPost | ProcessedPage
-
-  interface MetadataStore {
-    posts: ProcessedPost[]
-    pages: ProcessedPage[]
-  }
-
   interface Window {
     DISQUS: unknown
   }
